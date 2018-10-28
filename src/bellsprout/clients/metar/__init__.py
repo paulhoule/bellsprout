484ba5e3d5daef8927e4f74ecb4cad42da1b0beb
Paul Houle <paul@ontology2.com>
2018-10-28 17:59:46 -0400

    remove commented out lines

--- src/bellsprout/clients/metar/__init__.py
+++ src/bellsprout/clients/metar/__init__.py
@@ -59,34 +59,4 @@ def internal_metar(wxd):
     return document
 
 
-    # if self.press:
-    #     lines.append("pressure: %s" % self.press.string("mb"))
-    # if self.weather:
-    #     lines.append("weather: %s" % self.present_weather())
-    # if self.sky:
-    #     lines.append("sky: %s" % self.sky_conditions("\n     "))
-    # if self.press_sea_level:
-    #     lines.append("sea-level pressure: %s" % self.press_sea_level.string("mb"))
-    # if self.max_temp_6hr:
-    #     lines.append("6-hour max temp: %s" % str(self.max_temp_6hr))
-    # if self.max_temp_6hr:
-    #     lines.append("6-hour min temp: %s" % str(self.min_temp_6hr))
-    # if self.max_temp_24hr:
-    #     lines.append("24-hour max temp: %s" % str(self.max_temp_24hr))
-    # if self.max_temp_24hr:
-    #     lines.append("24-hour min temp: %s" % str(self.min_temp_24hr))
-    # if self.precip_1hr:
-    #     lines.append("1-hour precipitation: %s" % str(self.precip_1hr))
-    # if self.precip_3hr:
-    #     lines.append("3-hour precipitation: %s" % str(self.precip_3hr))
-    # if self.precip_6hr:
-    #     lines.append("6-hour precipitation: %s" % str(self.precip_6hr))
-    # if self.precip_24hr:
-    #     lines.append("24-hour precipitation: %s" % str(self.precip_24hr))
-    # if self._remarks:
-    #     lines.append("remarks:")
-    #     lines.append("- " + self.remarks("\n- "))
-    # if self._unparsed_remarks:
-    #     lines.append("- " + ' '.join(self._unparsed_remarks))
-
 
